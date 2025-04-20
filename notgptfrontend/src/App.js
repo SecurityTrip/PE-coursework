06517b47f80a60c6c7fd5e0488ab88e05fba2707
nikita <nikipagmal@gmail.com>
2025-04-21 02:10:30 +0300

    две страницы фронта

--- notgptfrontend/src/App.js
+++ notgptfrontend/src/App.js
@@ -1,42 +1,114 @@
-﻿//import logo from './logo.svg';
-import './App.css';
+﻿import './App.css';
+import React, { useState } from 'react';
 import logolabelimg from './лого_надпись.png';
 import logoimgimg from './лого_корабль.png';
+import { BrowserRouter as Router, Route, Routes, useNavigate,Link } from 'react-router-dom';
+import avaimg0 from './avas/avaimg0.gif';
+import avaimg1 from './avas/avaimg1.gif';
+import avaimg2 from './avas/avaimg2.gif';
+import avaimg3 from './avas/avaimg3.gif';
+import avaimg4 from './avas/avaimg4.gif';
+import avaimg5 from './avas/avaimg5.gif';
+import avaimg6 from './avas/avaimg6.gif';
+import avaimg7 from './avas/avaimg7.gif';
+import avaimg8 from './avas/avaimg8.gif';
+import avaimg9 from './avas/avaimg9.gif';
 function App() {
-    function handleClick(e) {
-        alert('Была нажата ссылка.');
-    }
     return (
-        <div className="App">
-            <div class="bckgr" ></div>
-            <img class="logolabel" src={logolabelimg}></img>
-            <img class="logoimg" src={logoimgimg}></img>
-            <header className="App-header">
-                {/*<img src={logo} className="App-logo" alt="logo" />*/}
-                {/*<p>*/}
-                {/*  Edit <code>src/App.js</code> and save to reload.*/}
-                {/*</p>*/}
-                {/*<a*/}
-                {/*  className="App-link"*/}
-                {/*  href="https://reactjs.org"*/}
-                {/*  target="_blank"*/}
-                {/*  rel="noopener noreferrer"*/}
-                {/*>*/}
-                {/*  Learn React*/}
-                {/*</a>*/}
-                <div class="logintab">
-                    <div style={{ fontSize: '170%', fontWeight: 'bold', color: 'rgb(0,0,0)', marginLeft: '30px', marginRight: '30px' }}>Авторизация</div>
-                    <div class="logintabdark">
-                        <input type="text" placeholder="Логин"/><br/>
-                        <input type="text" placeholder="Пароль"/><br/>
-                        <button>Войти</button>
-                    </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Auth />} />
+                    <Route path="/regis" element={<Regis />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+function Auth() {
+    const navigate = useNavigate(); // Теперь хук используется внутри компонента, который внутри Router
+
+    function handleClick() {
+        navigate('/menu'); // Навигация на другую страницу
+    }
+
+    return (
+        <header className="App-header">
+            <div className="bckgr"></div>
+            <img className="logolabel" src={logolabelimg} alt="Logo" />
+            <img className="logoimg" src={logoimgimg} alt="Logo" />
+            <div className="logintab">
+                <div
+                    style={{
+                        fontSize: '170%',
+                        fontWeight: 'bold',
+                        color: 'rgb(0,0,0)',
+                        marginLeft: '30px',
+                        marginRight: '30px',
+                    }}
+                >
+                    Авторизация
                 </div>
-                {/*<button onClick={handleClick}>*/}
-                {/*    fasdf*/}
-                {/*</button>*/}
-            </header>
-        </div>
+                <div className="logintabdark">
+                    <input type="text" placeholder="Логин"/>
+                    <br />
+                    <input type="text" placeholder="Пароль" />
+                    <br />
+                    <button onClick={handleClick}>Войти</button>
+                </div>
+            </div>
+            <Link to="/regis">Зарегистрироваться</Link>
+        </header>
+    );
+}
+
+function Regis() {
+    const navigate = useNavigate();
+
+    const [selavx, setSelavx] = useState(-3.5);
+    const [selavy, setSelavy] = useState(5.5);
+    function handleClick() {
+        navigate('/menu'); // Навигация на другую страницу
+    }
+    
+    return (
+
+        <header>
+            <div className="bckgr"></div>
+            <img className="logolabel" src={logolabelimg} alt="Logo" />
+            <img className="logoimg" src={logoimgimg} alt="Logo" />
+            <div className="regtab">Регистрация</div>
+            <div className="regtabdark"><input type="text" placeholder="Логин" class="reginputlog" />
+                <input type="text" placeholder="Пароль" class="reginputpass" />
+                <input type="text" placeholder="Повторите пароль" class="reginputpasspass" />
+                <button onClick={handleClick} class="regbutton">Зарегистрироваться</button>
+                <div className="avatarLabel">Выберите аватар:</div>
+                <SelectedAva x={selavx} y={selavy}></SelectedAva>
+                <Avatimbut y='7' x='-2' img={avaimg0} onClick={() => { setSelavx(-3.5); setSelavy(5.5); }} />
+                <Avatimbut y='7' x='8' img={avaimg1} onClick={() => { setSelavx(6.5); setSelavy(5.5); }} />
+                <Avatimbut y='7' x='18' img={avaimg2} onClick={() => { setSelavx(16.5); setSelavy(5.5); }} />
+                <Avatimbut y='7' x='28' img={avaimg3} onClick={() => { setSelavx(26.5); setSelavy(5.5); }} />
+                <Avatimbut y='7' x='38' img={avaimg4} onClick={() => { setSelavx(36.5); setSelavy(5.5); }} />
+                <Avatimbut y='17' x='-2' img={avaimg5} onClick={() => { setSelavx(-3.5); setSelavy(15.5); }} />
+                <Avatimbut y='17' x='8' img={avaimg6} onClick={() => { setSelavx(6.5); setSelavy(15.5); }} />
+                <Avatimbut y='17' x='18' img={avaimg7} onClick={() => { setSelavx(16.5); setSelavy(15.5); }} />
+                <Avatimbut y='17' x='28' img={avaimg8} onClick={() => { setSelavx(26.5); setSelavy(15.5); }} />
+                <Avatimbut y='17' x='38' img={avaimg9} onClick={() => { setSelavx(36.5); setSelavy(15.5); }} />
+            </div>
+            <Link to="/" className="linkToLogin">У меня уже есть аккаунт, войти</Link>
+            
+        </header>
+    );
+}
+function Avatimbut({ y, x, img,onClick }) {
+    return (
+        <button className="avatarimgbut" onClick={ onClick} style={{ top: y + 'vh', left: 'calc(50% + ' + x + 'vh', backgroundImage: 'url(' + img + ')', backgroundSize: 'cover'  } }></button>
+    );
+}
+function SelectedAva({y,x }) {
+    return (
+        <div className="selectedAva" style={{ top: y + 'vh', left: 'calc(50% + ' + x + 'vh' }} ></div>
     );
 }
 
